5e48564af0e5b472373a875fc3a5f336753299bd
gal-sev <dk58554@gmail.com>
2023-08-02 13:24:00 +0300

    Added the tagmul injection to contentScript

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,8 @@
 (() => {
-
+    let tagmulValue = 50;
+    let tagmulText = document.createElement("p");
+    tagmulText.textContent = `Tagmul: ${tagmulValue}`;
+    tagmulText.id = "product-tagmul";
+    let itemTitle = document.getElementById("product-page-prodict-title");
+    itemTitle.appendChild(tagmulText);
 })();
